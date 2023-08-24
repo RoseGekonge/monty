d3cbca350b1832c90a4c49fd6e1cbb2d1e9e52a6
RoseGekonge <rgekonge2001@gmail.com>
2023-08-24 12:47:18 -0700

    opcodes and bytecodes

--- monty.h
+++ monty.h
@@ -1,5 +1,18 @@
 #ifndef _MONTY_H_
 #define _MONTY_H_
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -31,6 +44,42 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+char *get_data_mode(void);
+char **get_lines(void);
+void exit_program(int status);
+void clean_up_program(void);
 
+/* Opcode handlers */
+
+void mty_op_push(stack_t **stack, unsigned int line_number);
+void mty_op_pall(stack_t **stack, unsigned int line_number);
+void mty_op_pint(stack_t **stack, unsigned int line_number);
+void mty_op_pop(stack_t **stack, unsigned int line_number);
+void mty_op_swap(stack_t **stack, unsigned int line_number);
+void mty_op_add(stack_t **stack, unsigned int line_number);
+void mty_op_nop(stack_t **stack, unsigned int line_number);
+void mty_op_sub(stack_t **stack, unsigned int line_number);
+void mty_op_div(stack_t **stack, unsigned int line_number);
+void mty_op_mul(stack_t **stack, unsigned int line_number);
+void mty_op_mod(stack_t **stack, unsigned int line_number);
+void mty_op_pchar(stack_t **stack, unsigned int line_number);
+void mty_op_pstr(stack_t **stack, unsigned int line_number);
+void mty_op_rotl(stack_t **stack, unsigned int line_number);
+void mty_op_rotr(stack_t **stack, unsigned int line_number);
+void mty_op_stack(stack_t **stack, unsigned int line_number);
+void mty_op_queue(stack_t **stack, unsigned int line_number);
+
+/**
+ * enum Date_Format_Modes - The data format modes for this program.
+ * @DF_LIFO: The data format code for a LIFO structure (like a stack)
+ * @DF_FIFO: The data format code for a FIFO structure (like a queue)
+ */
+enum Date_Format_Modes
+{
+	/* The data format code for a LIFO structure (like a stack) */
+	DF_LIFO,
+	/* The data format code for a FIFO structure (like a queue) */
+	DF_FIFO
+};
 
 #endif
